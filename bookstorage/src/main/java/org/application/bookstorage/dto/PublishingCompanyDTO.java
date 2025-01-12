9f4b02f7943841f2d6c7f5dcc7e1089c02e11cd4
Сафаров Д. <saf.kyomail@gmail.com>
2025-01-12 15:49:25 +0300

    Внедрены новые типы данных БД в бекэнд
    csv пока спит

--- bookstorage/src/main/java/org/application/bookstorage/dto/PublishingCompanyDTO.java
+++ bookstorage/src/main/java/org/application/bookstorage/dto/PublishingCompanyDTO.java
@@ -4,12 +4,14 @@ import lombok.Data;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDate;
+
 @Data
 public class PublishingCompanyDTO {
     @NotBlank(message = "Название издательства не может быть пустым")
     private String name;
 
-    private int establishmentYear;
+    private LocalDate establishmentYear;
 
     private String contactInfo;
 
